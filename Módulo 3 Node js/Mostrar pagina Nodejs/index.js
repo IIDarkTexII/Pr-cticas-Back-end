29cc65b0c6a5fb558c6a2a728a948cf51715bf0a
Alejandro Texis <texis369alejandro@gmail.com>
2022-12-03 00:10:15 -0600

    Node Js

---
+++ Módulo 3 Node js/Mostrar pagina Nodejs/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+router.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname + '/templates/index.html'))
+});
+
+router.get("/contacto", (req,res) => {
+    res.sendFile(path.join(__dirname + '/templates/contacto.html'))
+});
+
+router.get("/perfil", (req,res) => {
+    res.sendFile(path.join(__dirname + '/templates/perfil.html'))
+});
+
+app.post("/", (req,res)=>{
+    res.send("El usuario " + req.body.first_name + " ha sido registrado con el correo: " + req.body.email_adress);
+});
+app.use("/", router);
+app.listen(process.env.port || 8080);
+
+console.log("Activo en el puerto 8080")
